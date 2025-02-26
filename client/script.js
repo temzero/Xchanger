d2583b14b01f246d91c4fa0a64d35c6cc72624be
temzero <nguyentrannhan.00@gmail.com>
2025-02-26 16:00:37 +0700

    Xchanger

---
+++ client/script.js
@@ -0,0 +1,243 @@
+let callIndex = -1
+const states = [];
+
+function useState(initialValue) {
+    callIndex++
+
+    const currentIndex = Number(callIndex)
+    
+    if (states[currentIndex] === undefined) {
+        states[currentIndex] = initialValue;
+    }
+    
+    const setState = function (newValue) {
+        if (typeof newValue === 'function') {
+            states[currentIndex] = newValue(states[currentIndex]);
+        } else {
+            states[currentIndex] = newValue;
+        }
+        renderComponent();
+    };
+
+    return [() => states[currentIndex], setState];
+}
+
+
+let [currencyData, setCurrencyData] = useState([]);
+let [selectedCurrencies, setSelectedCurrencies] = useState({});
+
+function fetchCurrencyData() {
+    return fetch("http://localhost:4000/api/currencies")
+        .then(response => response.json())
+        .then(data => {
+            setCurrencyData(data);
+            setSelectedCurrencies({
+                currency1: data[1],
+                currency2: data[0]
+            });
+            renderComponent();
+        })
+        .catch(error => console.error("Error fetching currency data:", error));
+}
+
+fetchCurrencyData();
+// Update prices every 60 minutes
+setInterval(fetchCurrencyData, 3600888);
+
+// UI elements
+const input1 = document.getElementById('input-1');
+const input2 = document.getElementById('input-2');
+const rateDisplay = document.getElementById('rate-display');
+const currencyDisplay1 = document.getElementById('currency-1');
+const currencyDisplay2 = document.getElementById('currency-2');
+const dropdown1 = document.getElementById('dropdown-currency-1');
+const dropdownBtn1 = document.getElementById('currency-btn-1');
+const dropdownCurrency1 = document.getElementById('dropdown-currency-1');
+const dropdown2 = document.getElementById('dropdown-currency-2');
+const dropdownBtn2 = document.getElementById('currency-btn-2');
+const dropdownCurrency2 = document.getElementById('dropdown-currency-2');
+const convertBtn = document.getElementById('convert-btn');
+const reverseBtn = document.querySelector(".reverse-btn");
+
+setTimeout(() => {
+    // console.log('states', states)
+    // console.log('currencyData', currencyData())
+    handleCurrencyDropdown(dropdownCurrency1, dropdown1)
+    handleCurrencyDropdown(dropdownCurrency2, dropdown2)
+    dropdownBtnClick(dropdownBtn1, dropdown1, dropdown2);
+    dropdownBtnClick(dropdownBtn2, dropdown2, dropdown1);
+}, 100);
+
+// Re-render function (updates UI based on state)
+function renderComponent() {
+    // reset callIndex
+    callIndex = -1
+    const currencies = selectedCurrencies()
+
+    displayCurrency(currencies);
+    updateRateDisplay(currencies);
+    recalculateExchange(currencies);
+    synchronizeInputs(input1, input2, currencies.currency1, currencies.currency2);
+    synchronizeInputs(input2, input1, currencies.currency2, currencies.currency1);
+    
+    alwaysPositive(input1);
+    alwaysPositive(input2);
+}
+
+function handleCurrencyDropdown(dropdownCurrency, dropdown) {
+    if (!currencyData() || currencyData().length < 2) {
+        // console.warn("Currency data is empty or missing.");
+        return;
+    }
+
+    dropdownCurrency.innerHTML = currencyData().map((currency, index) => {
+        return `<li id="${index}"> 
+            <img src="${currency.icon}" class="currency-icon"> ${currency.code}
+        </li>`;
+    }).join('');
+
+    dropdownCurrency.addEventListener('click', e => {
+        e.stopPropagation();
+        const target = e.target.closest('li');
+        const selectedIndex = target.id;
+
+        setSelectedCurrencies(prev => ({
+            currency1: dropdownCurrency.id === 'dropdown-currency-1' ? currencyData()[selectedIndex] : prev.currency1,
+            currency2: dropdownCurrency.id === 'dropdown-currency-2' ? currencyData()[selectedIndex] : prev.currency2
+        }));
+
+        dropdown.style.display = 'none';
+    });
+}
+
+function dropdownBtnClick(dropdownBtn, dropdown, otherDropdown = null) {
+    dropdownBtn.addEventListener('click', e => {
+        e.stopPropagation();
+        if (otherDropdown) {
+            otherDropdown.style.display = 'none';
+        }
+        dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+    })
+}
+
+function recalculateExchange(selectedCurrencies) {
+    const { currency1, currency2 } = selectedCurrencies;
+
+    if (!currency1 || !currency2 || !currency1.icon || !currency2.icon) {
+        // console.warn("Currency data is empty or missing.");
+        return; 
+    }
+
+    if (input1.value !== '') {
+        input2.value = (parseFloat(input1.value) * currency1.price / currency2.price);
+    } else {
+        input2.value = '';
+    }
+
+    if (input2.value === '') {
+        input1.value = '';
+    }
+}
+
+// Function to update rate display
+function updateRateDisplay(selectedCurrencies) {
+    const { currency1, currency2 } = selectedCurrencies;
+
+    if (!currency1 || !currency2 || !currency1.icon || !currency2.icon) {
+        // console.warn("Currency data is empty or missing.");
+        return; 
+    }
+    
+    rateDisplay.innerHTML = `
+        1 
+        ${currency1.name} = ${(currency1.price / currency2.price).toLocaleString('en-US', { maximumFractionDigits: 6 })}
+        ${currency2.name}
+    `;
+    
+    input1.focus();
+    
+    // Set aria-placeholder for both input fields
+    input1.setAttribute('placeholder', '1');
+    input2.setAttribute('placeholder', (currency1.price / currency2.price).toLocaleString('en-US', { maximumFractionDigits: 6 }));
+}
+
+// Function to display selected currencies
+function displayCurrency(selectedCurrencies) {
+    const { currency1, currency2 } = selectedCurrencies;
+
+    if (!currency1 || !currency2 || !currency1.icon || !currency2.icon) {
+        // console.warn("Currency data is empty or missing.");
+        return; 
+    }
+
+    currencyDisplay1.innerHTML = `<img src="${currency1.icon}" class="currency-icon"> ${currency1.code}`;
+    currencyDisplay2.innerHTML = `<img src="${currency2.icon}" class="currency-icon"> ${currency2.code}`;
+}
+
+// Ensure inputs are always non-negative while typing
+function alwaysPositive(input) {
+    input.addEventListener('input', () => {
+        if (input.value < 0) {
+            input.value = 0;
+        }
+    });
+}
+
+function synchronizeInputs(typingInput, targetInput, typingCurrency, targetCurrency) {
+    typingInput.addEventListener('input', () => {
+        let value = parseFloat(typingInput.value);
+
+        if (document.activeElement !== typingInput) {
+            return; // Only update when user is actively typing
+        }
+
+        if (typingInput.value === '' || value < 0) {
+            targetInput.value = '';
+            return;
+        }
+
+        if (!isNaN(value)) {
+            const rate = typingCurrency.price / targetCurrency.price;
+            targetInput.value = (value * rate).toFixed(2);
+        }
+
+        if (value === 0) {
+            targetInput.value = 0;
+        }
+    });
+}
+
+
+// key input event
+document.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'Tab':
+            e.preventDefault();
+            if (document.activeElement === input1) {
+                input2.focus();
+            } else {
+                input1.focus();
+            }
+            break;
+        case ' ':
+            e.preventDefault();
+            document.getElementById('reverse-btn').click();
+            break;
+    }
+});
+
+document.addEventListener('click', e => {
+    dropdown1.style.display = 'none'
+    dropdown2.style.display = 'none'
+})
+
+reverseBtn.addEventListener('click', function () {
+    this.classList.toggle('rotate');
+
+    setSelectedCurrencies(prev => ({
+        currency1: prev.currency2,
+        currency2: prev.currency1
+    }));
+
+    input1.focus();
+});
---
+++ server/server.js
@@ -0,0 +1,162 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const API_URL_CRYPTO = process.env.API_URL_CRYPTO;
+const API_URL_FOREX = process.env.API_URL_FOREX;
+const API_URL_GOLD = process.env.API_URL_GOLD;
+const API_URL_SILVER = process.env.API_URL_SILVER;
+
+const cryptoCurrencies = [
+    ['USD Coin', 'USDC', 1],
+    ['Bitcoin', 'BTC'],
+    ['Ethereum', 'ETH'],
+    ['Cardano', 'ADA'],
+    ['Dogecoin', 'DOGE'],
+    ['Solana', 'SOL'],
+    ['Ripple', 'XRP'],
+    ['Polkadot', 'DOT'],
+    ['Litecoin', 'LTC'],
+    ['Chainlink', 'LINK'],
+    ['Worldcoin', 'WLD'],
+    ['BinanceCoin', 'BNB'],
+    ['Monero', 'XMR'],
+    ['Shiba-Inu', 'SHIB'],
+    ['Uniswap', 'UNI'],
+    ['Tezos', 'XTZ'],
+    ['Cosmos', 'ATOM'],
+    ['VeChain', 'VET'],
+    ['Stellar', 'XLM'],
+    ['Tron', 'TRX'],
+    ['Filecoin', 'FIL'],
+    ['EOS', 'EOS'],
+    ['Near', 'NEAR'],
+    ['Aptos', 'APT'],
+    ['The-Graph', 'GRT'],
+    ['Maker', 'MKR'],
+    ['Fantom', 'FTM'],
+    ['Arbitrum', 'ARB'],
+    ['Internet-Computer', 'ICP'],
+];
+
+const fiatCurrencies = [
+    ['Australian Dollar', 'AUD'],
+    ['Brazilian Real', 'BRL'],
+    ['British Pound', 'GBP'],
+    ['Canadian Dollar', 'CAD'],
+    ['Chinese Yuan', 'CNY'],
+    ['Euro', 'EUR'],
+    ['Indian Rupee', 'INR'],
+    ['Indonesian Rupiah', 'IDR'],
+    ['Japanese Yen', 'JPY'],
+    ['Mexican Peso', 'MXN'],
+    ['New Zealand Dollar', 'NZD'],
+    ['Russian Ruble', 'RUB'],
+    ['Saudi Riyal', 'SAR'],
+    ['Singapore Dollar', 'SGD'],
+    ['South African Rand', 'ZAR'],
+    ['South Korean Won', 'KRW'],
+    ['Swiss Franc', 'CHF'],
+    ['Vietnam Dong', 'VND']
+];
+
+const commodities = [
+    ['ounce Silver', 'SILVER'],
+    ['ounce Gold', 'GOLD']
+];
+
+const createCurrencyData = (currencies) =>
+    currencies.map(([name, code, price]) => ({
+        name,
+        code,
+        icon: `tokens/${code}.svg`,
+        ...(price !== undefined && { price })
+    }));
+
+const currencyData = [
+    ...createCurrencyData(cryptoCurrencies),
+    ...createCurrencyData(fiatCurrencies),
+    ...createCurrencyData(commodities)
+];
+
+
+function fetchCryptoPrices() {
+    return fetch(API_URL_CRYPTO)
+        .then(response => response.json())
+        .then(data => {
+            currencyData.forEach(currency => {
+                const lowerCaseName = currency.name.toLowerCase();
+                if (data[lowerCaseName]?.usd) {
+                    currency.price = data[lowerCaseName].usd;
+                }
+            });
+            console.log("Crypto prices updated.");
+        })
+        .catch(error => console.error("Error fetching crypto prices:", error));
+}
+
+function fetchForexRates() {
+    return fetch(API_URL_FOREX)
+        .then(response => response.json())
+        .then(data => {
+            if (data.rates) {
+                currencyData.forEach(currency => {
+                    if (data.rates[currency.code]) {
+                        currency.price = 1 / data.rates[currency.code];
+                    }
+                });
+            }
+            console.log("Forex prices updated.");
+        })
+        .catch(error => console.error("Error fetching forex prices:", error));
+}
+
+function fetchMetalPrices() {
+    const metalURLs = [API_URL_SILVER, API_URL_GOLD];
+
+    return Promise.allSettled(metalURLs.map(metalURL =>
+        fetch(metalURL)
+            .then(response => response.json())
+            .then(data => {
+                currencyData.forEach(currency => {
+                    if (data.name.toLowerCase() === currency.code.toLowerCase()) {
+                        currency.price = data.price;
+                    }
+                });
+                console.log(`${data.name} price updated.`);
+            })
+            .catch(error => console.error(`Error fetching metals price:`, error))
+        ));
+}
+
+
+async function updatePrices() {
+    await Promise.allSettled([
+        fetchCryptoPrices(),
+        fetchForexRates(),
+        fetchMetalPrices()
+    ]);
+}
+
+// Update prices every 60 minutes
+setInterval(updatePrices, 3600888);
+
+// Initial Fetch
+updatePrices();
+
+app.get("/", (req, res) => {
+    res.send("Currency database server is running");
+});
+
+// API endpoint to fetch all currency data
+app.get("/api/currencies", (req, res) => {
+    res.json(currencyData);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
